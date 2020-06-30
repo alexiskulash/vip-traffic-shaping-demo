27cc77ed241f8eb1a28274689b94bdfd8c3baa3a
Alexis Kulash <alexis.kulash@automattic.com>
2020-06-30 18:04:32 +0000

    Outlining some basic plugin files

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# vip-traffic-shaping-demo
+# VIP Traffic Shaping Demo
+==================
+
+A very simple demo of various mechanisms for configuring traffic on the VIP Go platform.
+
+Features include:
+
+- TBD
+- TBD
+- TBD
+
+# Installation
+------------
+
+To install and configure this plugin...
+
+1. Commit the plugin to the /plugins directory in your site repository
+2. Activate the plugin via code using `wpcom_vip_load_plugin()` (recommended) or manually via the 'Plugins' admin menu
+3. Configure the settings on the VIP Traffic Shaping Demo settings page
+4. TBD
---
+++ vip-traffic-shaping-demo.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * Plugin Name: VIP Traffic Shaping Demo
+ * Description: This plugin demonstrates various mechanisms for configuring traffic on the VIP Go platform.
+ * Version: 1.0
+ * Author: Alexis Kulash, Matt Perry, Automattic
+ */
+
+if ( class_exists( 'VIP_Traffic_Shaping_Demo' ) ) {
+	return;
+}
+
+class VIP_Traffic_Shaping_Demo {
+
+	/**
+	 * Initiate an instance of this class if one doesn't exist already.
+	 *
+	 * @return VIP_Traffic_Shaping_Demo instance
+	 */
+	static public function init() {
+		static $instance = false;
+
+		if ( ! $instance ) {
+			$instance = new VIP_Jetpack_Sync_Cron;
+		}
+
+		return $instance;
+	}
+
+	/**
+	 * Class constructor for hooking actions/filters.
+	 *
+	 * @return void
+	 */
+	public function __construct() {
+		// Do stuff here
+	}
+}
+
+add_action( 'init', [ 'VIP_Traffic_Shaping_Demo', 'init' ] );
